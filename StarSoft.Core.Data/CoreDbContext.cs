03564f8743184d12dbca1022f78eeaf9938c87c0
Star Softworks Master Chief <david.ortega.m@outlook.com>
2024-02-12 16:41:19 -0500

    Agregadas librerías y dependencias

---
+++ StarSoft.Core.Data/CoreDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StarSoft.Core.Data
+{
+    public class CoreDbContext : DbContext
+    {
+        public CoreDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
--- StarSoft.Core/Program.cs
+++ StarSoft.Core/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+builder.Services.AddWebOptimizer();
 
 var app = builder.Build();
 
@@ -29,6 +30,7 @@ else
 }
 
 app.UseHttpsRedirection();
+app.UseWebOptimizer();
 app.UseStaticFiles();
 
 app.UseRouting();
@@ -37,7 +39,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
